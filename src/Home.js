2a39260b93c8a5598ab2de00f5f3c34f76586d8b
tornike Despotashvili <tornike.despotashvili@gmail.com>
2022-11-09 03:33:20 +0400

    Your commit message

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,26 @@ import {Link} from  "react-router-dom";
 import './home.css'
 import './navbar.css'
 import './about.css'
+import './project.css'
+import './contact.css'
+import './footer.css'
 
 //images
 import despoIMG from './images/despo_img.png'
-import menu from './images/menu.png'
-import close from './images/close.png'
+import menu     from './images/menu.png'
+import close    from './images/close.png'
+import carco    from './images/carco_prj.png'
+import dlab     from './images/3dlab.png'
+import pixnft   from './images/pixnft.png'
+import indian   from './images/indian.png'
+import linkedin from './images/linkedin.svg'
+import facebook from './images/facebook.svg'
+import github   from './images/github.svg'
+import twitter  from './images/twitter.svg'
+import linkedLi from './images/linkedin-light.png'
+import twitterLi from './images/twitter-light.png'
+import facebookLi from './images/facebook-light.png'
+import githubLi from './images/github-light.png'
 
 const imageStyle = {
     width: '50px',
@@ -21,23 +36,37 @@ function Home() {
     const [toggle, setToggle]   = useState(false)
     const [menuIMG, setMenuIMG] = useState(menu)
     const [dashbrd, setDashbrd] = useState('menu_dashboard')
+    const [overlay, setOverlay] = useState('overlay')
 
     function toggleMenu() {
         if(!toggle){
             setMenuIMG(close)
             setDashbrd('menu_dashboard visible')
+            setOverlay('overlay visible')
             setToggle(true)
         }else{
             setMenuIMG(menu)
             setDashbrd('menu_dashboard')
+            setOverlay('overlay')
             setToggle(false)
         }
     }
 
-    const homeRef = useRef(null)
+    const homeRef    = useRef(null)
+    const aboutRef   = useRef(null)
+    const projectRef = useRef(null)
+    const contactRef = useRef(null)
     const handleClick = (refname) => {
         refname.current?.scrollIntoView({behavior: 'smooth'});
       };
+
+    const redirect = (url) => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }  
+
+    const socialLink = (url) => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
 
     return(
     <div style={{width:'100%', height:'100vh'}}>
@@ -51,9 +80,9 @@ function Home() {
 
             <div className="nav_right">
                 <Link to='' className="navLinks" onClick={()=> handleClick(homeRef)}>Home</Link>
-                <Link to='' className="navLinks">About</Link>
-                <Link to='' className="navLinks">Projects</Link>
-                <Link to='' className="navLinks">Contact</Link>
+                <Link to='' className="navLinks" onClick={()=> handleClick(aboutRef)}>About</Link>
+                <Link to='' className="navLinks" onClick={()=> handleClick(projectRef)}>Projects</Link>
+                <Link to='' className="navLinks" onClick={()=> handleClick(contactRef)}>Contact</Link>
             </div>
 
             <div className="burger_menu">
@@ -62,29 +91,44 @@ function Home() {
         </nav>
 
         <div className={dashbrd} onClick={()=> toggleMenu()}>
-            <div><Link to='' className="navLinks">Home</Link></div>
-            <div><Link to='' className="navLinks">About</Link></div>
-            <div><Link to='' className="navLinks">Projects</Link></div>
-            <div><Link to='' className="navLinks">Contact</Link></div>
+            <div><Link to='' className="navLinks" onClick={()=> handleClick(homeRef)}>Home</Link></div>
+            <div><Link to='' className="navLinks" onClick={()=> handleClick(aboutRef)}>About</Link></div>
+            <div><Link to='' className="navLinks" onClick={()=> handleClick(projectRef)}>Projects</Link></div>
+            <div><Link to='' className="navLinks" onClick={()=> handleClick(contactRef)}>Contact</Link></div>
         </div>
 
+        <div className={overlay} onClick={()=> toggleMenu()}></div>
+
+        
+
         {/* ჰიუმ ფეიჯი ////////////////////////////////////////////////////// */}
-        <div className="home" ref={homeRef}>
+        <span ref={homeRef}></span>
+        <div className="home" >
             <h1>HEY, I'M TORNIKE DESPOTASHVILI</h1>
 
             <div className="home_p_txt">
                 <p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications <br/> that leads to the success of the overall product</p>
             </div>
 
-            <div className="button_to_prj">
+            <div className="button_to_prj" onClick={()=> handleClick(projectRef)}>
                 <div className="button_to_prj_inner">
                     <p>PROJECTS</p>
                 </div>
             </div>
+
+            {/* სოციალური ქსელები */}
+            <span className="social">
+                <img className="social_img" src={linkedin} onClick={()=> socialLink("https://www.linkedin.com/in/tornike-despotashvili-250150219/")} />
+                <img className="social_img" src={twitter} onClick={()=> socialLink("https://twitter.com/TDespotashvili")} />
+                <img className="social_img" src={facebook} onClick={()=> socialLink("https://www.facebook.com/tornike.despotashvili/")} />
+                <img className="social_img" src={github} onClick={()=> socialLink("https://github.com/Desp0o")} />
+            </span>
+            
         </div>
 
-        <section>
-            <div className="about_me">
+        {/* ებაუთ ფეიჯი ////////////////////////////////////////////////////// */}
+        <span ref={aboutRef}></span>
+        <div className="about_me" >
                 <h1 className="about_me_h1">ABOUT ME</h1>
                 <span className="underline"></span>
 
@@ -97,7 +141,7 @@ function Home() {
                         I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.
                         </p>
 
-                        <div className="button_to_cont">
+                        <div className="button_to_cont" onClick={()=> handleClick(contactRef)}>
                             <div className="button_to_cont_inner">
                                 <p>CONTACT</p>
                         </div>
@@ -124,15 +168,149 @@ function Home() {
                         <div className="my_skills">
                             <span className="skill">Git</span>
                             <span className="skill">JQuery</span>
+                            <span className="skill">Docker</span>
+                            <span className="skill">PhotoShop</span>
                         </div>
                     </div>
                 </div>
-            </div>
+        </div>
             
-        </section>
-        
+        {/* ებაუთ ფეიჯი ////////////////////////////////////////////////////// */}
+        <span ref={projectRef}></span>
+        <div className="projects" >
+                <div className="header_underline">
+                    <h1 className="about_me_h1">PROJECTS</h1>
+                    <span className="underline"></span>
+                </div>
+
+            <div className="projects_inner">
+
+                <div className="prj">
+                        <div className="prj_img">
+                            <img className="project_image" src={carco} />
+                        </div>
+
+
+                        <div className="prj_inner">
+                            <h3>Carco.Ge</h3>
+                            <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.</p>
+                            <span className="button_to_cont visitBTN">
+                                <span className="button_to_cont_inner" onClick={()=> redirect('https://carco.ge/')}>Visit</span>
+                            </span>
+                        </div>
+                </div>
+
+                <div className="prj">
+                        <div className="prj_img">
+                            <img className="project_image" src={dlab} />
+                        </div>
+
+
+                        <div className="prj_inner">
+                            <h3>3dlab.Group</h3>
+                            <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.</p>
+                            <span className="button_to_cont visitBTN">
+                                <span className="button_to_cont_inner" onClick={()=> redirect('https://3dlab.group/')}>Visit</span>
+                            </span>
+                        </div>
+                </div>
+
+                <div className="prj">
+                        <div className="prj_img">
+                            <img className="project_image" src={pixnft} />
+                        </div>
+
+
+                        <div className="prj_inner">
+                            <h3>pixNFT.Me</h3>
+                            <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.</p>
+                            <span className="button_to_cont visitBTN">
+                                <span className="button_to_cont_inner" onClick={()=> redirect('https://pixnft.me/')}>Visit</span>
+                            </span>
+                        </div>
+                </div>
+
+                <div className="prj">
+                        <div className="prj_img">
+                            <img className="project_image" src={indian} />
+                        </div>
+
+
+                        <div className="prj_inner">
+                            <h2>Moto Shop Clone</h2>
+                            <p>Yubter is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.</p>
+                            <span className="button_to_cont visitBTN">
+                                <span className="button_to_cont_inner" onClick={()=>redirect('https://desp0o.github.io/IndianMotorcycle/')}>Visit</span>
+                            </span>
+                        </div>
+                </div>
+
+            </div>
+
+
+            <span className="button_to_cont seeMore">
+               <span className="button_to_cont_inner seeMoteBTN" onClick={()=>redirect('https://desp0o.github.io/IndianMotorcycle/')}>See More</span>
+            </span>
+
+
+
+        </div>
+
+        {/* კონტაქტი ///////////////////////////////////////////////////////////*/}
+        <span ref={contactRef}></span>
+        <div className="contact" >
+            <div className="contact_header">
+                <h1 className="about_me_h1">CONTACT</h1>
+                <span className="underline"></span>
+                <p style={{textAlign:'center', marginTop:'20px'}}>Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
+            </div>
+
+        <div className="form_parent">
+            <form action="https://formsubmit.co/8e6d01b034a735685229e1724634d364" method="POST">
+                <input className="simpleInput" type="text" name="name" placeholder="Name" required />
+                <input className="simpleInput" type="email" name="email" placeholder="Email" required />
+                <input className="simpleInput" type="text" name="number" placeholder="Number" required />
+                <input className="textarea" type="text" name="message" placeholder="Your Text" required />
+                <input type="hidden" name="_next" value="http://localhost:3000/" />
+                <input type="hidden" name="_captcha" value="false" />
+                <div className="button_to_cont submit_parent">
+                    <button className="submitBTN button_to_cont_inner" type="submit">Send</button>
+                </div>
+            </form>
+        </div>
+
+        </div>
+
+        {/* footer //////////////////////////////////////////////////////////// */}
+        <div className="footer">
+            <div className="footer_top">
+                <div className="footer_top_left">
+                    <p className="footer_hedaer">TORNIKE DESPOTASHVILI</p>
+                    <p className="footer_hedaer_txt" style={{color:'#fff'}}>A Frontend focused Web Developer building the Frontend of Websites and <br/>
+                    Web Applications that leads to the success of the overall product</p>
+                </div>
+
+                <div className="footer_top_right">
+                    <p className="footer_hedaer">SOCIAL</p>
+                    <span className="footer_social">
+                        <img className="footer_social_img" src={linkedLi} onClick={()=> socialLink("https://www.linkedin.com/in/tornike-despotashvili-250150219/")} />
+                        <img className="footer_social_img" src={twitterLi} onClick={()=> socialLink("https://twitter.com/TDespotashvili")} />
+                        <img className="footer_social_img" src={facebookLi} onClick={()=> socialLink("https://www.facebook.com/tornike.despotashvili/")} />
+                        <img className="footer_social_img" src={githubLi} onClick={()=> socialLink("https://github.com/Desp0o")} />
+                    </span>
+                </div>
+            </div>
+
+            <div className="footer_bottom">
+                <p style={{fontSize:'12px', color:'#fff', textAlign:'center'}}>©Made with love by Despo</p>
+            </div>
+        </div>
+
     </div>
     )
 }
 
 export default Home;
+
+
+    
