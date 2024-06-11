168fc88d7a3862a00a9fecf25f4385b0c1886692
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-11 15:42:29 +0400

    add new proj

--- src/components/ProjectArrayData.jsx
+++ src/components/ProjectArrayData.jsx
@@ -1,97 +1,143 @@
-import pixnft      from '../images/pixnft.webp'
-import indian      from '../images/indian.webp'
-import archic      from '../images/archic_prj.webp'
-import chess       from '../images/chess_prj.webp'
-import portfolio   from '../images/portfolio_prj.webp'
-import nexus       from '../images/nexus.webp'
-import carco       from '../images/carco_prj.webp'
-import dlab        from '../images/3dlab.webp'
-import magicMas    from '../images/magicMassage.webp'
+import pixnft from '../images/pixnft.webp'
+import indian from '../images/indian.webp'
+import archic from '../images/archic_prj.webp'
+import chess from '../images/chess_prj.webp'
+import portfolio from '../images/portfolio_prj.webp'
+import nexus from '../images/nexus.webp'
+import carco from '../images/carco_prj.webp'
+import dlab from '../images/3dlab.webp'
+import magicMas from '../images/magicMassage.webp'
 import tourGeorgia from '../images/tourGeorgia.webp'
-import shutter     from '../images/shutterpic.webp'
-import panorama    from '../images/panorama.webp'
-import sugarland   from '../images/sugarlandProject.webp'
-import gsgCover    from '../images/gsgCover.webp'
-import myVideoPop  from '../images/myvideoPopUp.webp'
-import moviedb     from "../images/moviedb.webp"
-import tvbox       from "../images/tvbox.webp"
-import moGraph     from "../images/mograph.webp"
-import evernote    from "../images/evernote-proj.webp"
+import shutter from '../images/shutterpic.webp'
+import panorama from '../images/panorama.webp'
+import sugarland from '../images/sugarlandProject.webp'
+import gsgCover from '../images/gsgCover.webp'
+import myVideoPop from '../images/myvideoPopUp.webp'
+import moviedb from "../images/moviedb.webp"
+import tvbox from "../images/tvbox.webp"
+import moGraph from "../images/mograph.webp"
+import evernote from "../images/evernote-proj.webp"
+import movieCom from "../images/movieCom.webp"
 
 export const ProjectArray = [
-    {id:1, image:`${carco}`, title:'Carco.Ge', description:'ავტო იმპორტის ვებ-გვერდი,ტრანსპორტირებისა და განბაჟების კალკულატორით.', button:'ნახვა',
-            descriptionEng:'Auto import Web page, with transportation and Customs Clearnece calculator.', buttonEng:'Visit',  link:'https://carco.ge/',
-            work1:'HTML', work2:'CSS', work3:'JS'},
-    
-    {id:2, image:`${panorama}`, title:'Twins Development', description:'Twins Development-ი სამშენებლო კომპანიის ვებ გვერდი.', button:'ნახვა',
-            descriptionEng:'Landing page fro Twins Development.', buttonEng:'Visit', link:'https://twins.com.ge/',
-            work1:'React JS', work2:'CSS', work3:'swiper.js'},
-    
-    {id:3, image:`${tvbox}`, title:'TVbox.Ge', description:'ვებ გვერდი Myvideo.Ge-ს ტელევიზიისთვის.', button:'ნახვა',
-            descriptionEng:'web page for Myvideo.Ge devices.', buttonEng:'Visit',  link:'https://tvbox.ge/',
-            work1:'React JS', work2:'CSS', work4:'Axios'},
+        {
+                id: 1, image: `${carco}`, title: 'Carco.Ge', description: 'ავტო იმპორტის ვებ-გვერდი,ტრანსპორტირებისა და განბაჟების კალკულატორით.', button: 'ნახვა',
+                descriptionEng: 'Auto import Web page, with transportation and Customs Clearnece calculator.', buttonEng: 'Visit', link: 'https://carco.ge/',
+                work1: 'HTML', work2: 'CSS', work3: 'JS'
+        },
 
-    {id:4, image:`${sugarland}`, title:'Sugarlandspamassage.com', description:'Sugarland Spa Massage - გამაჯანსაღებელი და   სამკურნალო მასაჟები.', button:'ნახვა',
-            descriptionEng:'Sugarland Spa Massage therapy Will bring luxury quality services.', buttonEng:'Visit', link:'https://sugarlandspamassage.com/',
-            work1:'React JS', work2:'CSS'},
-        
-    {id:18, image:`${evernote}`, title:'Evernote Lite', description:'შექმენით და შეინახეთ თქვენთვის საჭირო ფაილები, დოკუმენტები და ტასკები', button:'ნახვა',
-            descriptionEng:'Create, manage, and organize notes, posts, and tasks on your personal productivity platform.', buttonEng:'Visit',  link:'https://charming-bubblegum-0838fc.netlify.app/',
-            work1:'React JS', work2:'CSS', work3:'NodeJs', work4:'MySQL'},
+        {
+                id: 2, image: `${panorama}`, title: 'Twins Development', description: 'Twins Development-ი სამშენებლო კომპანიის ვებ გვერდი.', button: 'ნახვა',
+                descriptionEng: 'Landing page fro Twins Development.', buttonEng: 'Visit', link: 'https://twins.com.ge/',
+                work1: 'React JS', work2: 'CSS', work3: 'swiper.js'
+        },
 
-    {id:5, image:`${dlab}`, title:'3Dlab.Group', description:'ანიმაციური სტუდია, რომელიც ქმნის სხვადასხვა ტიპის 3D სარეკლამო ანიმაციებს კომპანიებისთვის.', button:'ნახვა',
-            descriptionEng:'3Dlab is Animation studio, famous in social networks, with its funny projects.', buttonEng:'Visit',  link:'https://desp0o.github.io/3dlab/',
-            work1:'HTML', work2:'CSS', work3:'JS', work4:'Bootstrap'},
+        {
+                id: 3, image: `${tvbox}`, title: 'TVbox.Ge', description: 'ვებ გვერდი Myvideo.Ge-ს ტელევიზიისთვის.', button: 'ნახვა',
+                descriptionEng: 'web page for Myvideo.Ge devices.', buttonEng: 'Visit', link: 'https://tvbox.ge/',
+                work1: 'React JS', work2: 'CSS', work4: 'Axios'
+        },
 
-    {id:6, image:`${tourGeorgia}`, title:'Tour Georgia', description:'უკრაინულ-ქართული ტურისტული კომპანია', button:'ნახვა',
-            descriptionEng:'Georgian-Ukrainian tour agency in Georgia.', buttonEng:'Visit', link:'https://tourgeorgia.com.ge/',
-            work1:'React JS', work2:'Tailwind', work3:'swiper.js'},
-                                                
-    {id:7, image:`${nexus}`, title:'Nexus Logistic LLC', description:'Nexus Logistics გადამზიდი კომპანია.', button:'ნახვა',
-            descriptionEng:'Nexus Logistics LLC is a 24/7 delivery/dispatching/carrier company.', buttonEng:'Visit', link:'https://desp0o.github.io/logistic/',
-            work1:'React JS', work2:'CSS'},
-    
-    {id:8, image:`${gsgCover}`, title:'gso-gsgo.ge', description:'საქართველოს ონკოლოგიის სკოლისა და საქართველოს გერიატრიული ონკოლოგიის საზოგადოების, საინფორმაციო ვე გვერდი.', button:'ნახვა',
-            descriptionEng:'Web page for Gerogian School of Oncology and Georgian Society of Geriatric Oncology.', buttonEng:'Visit', link:'https://gso-gsgo.ge/',
-            work1:'React JS', work2:'CSS'},
+        {
+                id: 14, image: `${movieCom}`, title: 'Movie Quiz', description: 'მიმდინარე პროექტი, სადაც მომხარებელს შეუძლია რეგისტრაცია და ავოტრიზაცია. მომხარებელს აქვს შესაძლებლობა განათავსოს პოსტები, გამოკითხევბი, განათავსოს და ასევე ითამაშოს ქვიზები და ქვესთები. თითოეულიი ამ აქტივობით იუზერი აგროვებს ქულებს, რომლითაც იღებს ონაწილეობას გათამაშებაში და შესაძლებლობა ექნება მოიგოს პრიზები და მიიღოს ეიჩივმენთები.', button: 'მიმდინარე',
+                descriptionEng: 'Ongoing project where users can register and authenticate. Users can create posts, polls, quizzes, and questions. They can like, save, and comment on posts. For each task, users gain points, which can help them win in ratings and get prizes. Users also earn achievements.', buttonEng: 'Ongoing', link: 'https://dev.devweb.cc/#/',
+                work1: 'React', work2: 'CSS', work3: 'React-redux', work4:"React-Query"
+        },
 
-    {id:9, image:`${myVideoPop}`, title:'PopUp For Myvideo.Ge', description:'არხების სიის პოპაპი Myvideo.Ge-სთვის', button:'ნახვა',
-            descriptionEng:'Channel list popup for Myvideo.Ge', buttonEng:'Visit', link:'https://desp0o.github.io/channelPopUp/',
-            work1:'Html', work2:'CSS', work3:'JS'},
 
-    {id:20, image:`${shutter}`, title:'shutter.pics', description:'პორტფოლიო ფოტოგრაფისთვის', button:'ნახვა',
-            descriptionEng:'Super light portfolio for photographer with image viewer', buttonEng:'Visit',  link:'https://meek-basbousa-38f2cc.netlify.app/',
-            work1:'React JS', work2:'CSS', work3:'Tailwind', work4:'Axios'},
+        {
+                id: 4, image: `${sugarland}`, title: 'Sugarlandspamassage.com', description: 'Sugarland Spa Massage - გამაჯანსაღებელი და   სამკურნალო მასაჟები.', button: 'ნახვა',
+                descriptionEng: 'Sugarland Spa Massage therapy Will bring luxury quality services.', buttonEng: 'Visit', link: 'https://sugarlandspamassage.com/',
+                work1: 'React JS', work2: 'CSS'
+        },
 
-    {id:19, image:`${moGraph}`, title:'moGraph', description:'მოუშენ დიზაინის კურსების ვებ გვერდი.', button:'ნახვა',
-            descriptionEng:'Web page for Courses of motion design', buttonEng:'Visit',  link:'https://desp0o.github.io/moGraph/',
-            work1:'HTML', work2:'CSS', work3:'js'},
+        {
+                id: 18, image: `${evernote}`, title: 'Evernote Lite', description: 'შექმენით და შეინახეთ თქვენთვის საჭირო ფაილები, დოკუმენტები და ტასკები', button: 'ნახვა',
+                descriptionEng: 'Create, manage, and organize notes, posts, and tasks on your personal productivity platform.', buttonEng: 'Visit', link: 'https://charming-bubblegum-0838fc.netlify.app/',
+                work1: 'React JS', work2: 'CSS', work3: 'NodeJs', work4: 'MySQL'
+        },
 
-    {id:10, image:`${indian}`, title:'Moto Shop Clone', description:'მოტოციკლეტების მაღაზიის პროექტი.', button:'ნახვა',
-            descriptionEng:'Moto shop web page clone, with responsive design and links animation.', buttonEng:'Visit', link:'https://desp0o.github.io/IndianMotorcycle/',
-            work1:'HTML', work2:'CSS', work3:'JS'},
+        {
+                id: 5, image: `${dlab}`, title: '3Dlab.Group', description: 'ანიმაციური სტუდია, რომელიც ქმნის სხვადასხვა ტიპის 3D სარეკლამო ანიმაციებს კომპანიებისთვის.', button: 'ნახვა',
+                descriptionEng: '3Dlab is Animation studio, famous in social networks, with its funny projects.', buttonEng: 'Visit', link: 'https://desp0o.github.io/3dlab/',
+                work1: 'HTML', work2: 'CSS', work3: 'JS', work4: 'Bootstrap'
+        },
 
-    {id:11, image:`${archic}`, title:'Architect Clone', description:'თანამედროვე,ესეთიკური, ტრადიციული ღირებულებების მქონე არქიტექტურული დიზაინები.', button:'ნახვა',
-            descriptionEng:'Architecutre web page clone, with responsive design and animations.', buttonEng:'Visit', link:'https://desp0o.github.io/Archic/',
-            work1:'HTML', work2:'CSS', work3:'JS'},
+        {
+                id: 6, image: `${tourGeorgia}`, title: 'Tour Georgia', description: 'უკრაინულ-ქართული ტურისტული კომპანია', button: 'ნახვა',
+                descriptionEng: 'Georgian-Ukrainian tour agency in Georgia.', buttonEng: 'Visit', link: 'https://tourgeorgia.com.ge/',
+                work1: 'React JS', work2: 'Tailwind', work3: 'swiper.js'
+        },
 
-    {id:12, image:`${chess}`, title:'Chess Championship', description:'ჭადრაკის ჩემპიონატის რეგისტრაციის ფორმა.', button:'ნახვა',
-            descriptionEng:'Chess registration form built with.', buttonEng:'Visit', link:'https://desp0o.github.io/RedBeryy-Chess-Hero/', 
-            work1:'React JS', work2:'CSS'},
+        {
+                id: 7, image: `${nexus}`, title: 'Nexus Logistic LLC', description: 'Nexus Logistics გადამზიდი კომპანია.', button: 'ნახვა',
+                descriptionEng: 'Nexus Logistics LLC is a 24/7 delivery/dispatching/carrier company.', buttonEng: 'Visit', link: 'https://desp0o.github.io/logistic/',
+                work1: 'React JS', work2: 'CSS'
+        },
 
-    {id:13, image:`${portfolio}`, title:'Devweb.cc', description:'პორტფოლიო ვებ დეველოპერისთვის.', button:'ნახვა',
-            descriptionEng:'Super light portfolio web page for Frontend Web Developer.', buttonEng:'Visit', link:'https://devweb.cc/', 
-            work1:'React JS', work2:'CSS'},
+        {
+                id: 8, image: `${gsgCover}`, title: 'gso-gsgo.ge', description: 'საქართველოს ონკოლოგიის სკოლისა და საქართველოს გერიატრიული ონკოლოგიის საზოგადოების, საინფორმაციო ვე გვერდი.', button: 'ნახვა',
+                descriptionEng: 'Web page for Gerogian School of Oncology and Georgian Society of Geriatric Oncology.', buttonEng: 'Visit', link: 'https://gso-gsgo.ge/',
+                work1: 'React JS', work2: 'CSS'
+        },
 
-    {id:16, image:`${magicMas}`, title:'MagicMassage', description:'მულტი ფუნქციური მასაჟორის საინფორმაციო ვებ გვერდიის კლონი.', button:'ნახვა',
-            descriptionEng:'MagicMassage™ provides relief from head tensions.', buttonEng:'Visit', link:'https://desp0o.github.io/MagicMassage/',
-            work1:'HTML', work2:'CSS', work3:'JS'},
+        {
+                id: 9, image: `${myVideoPop}`, title: 'PopUp For Myvideo.Ge', description: 'არხების სიის პოპაპი Myvideo.Ge-სთვის', button: 'ნახვა',
+                descriptionEng: 'Channel list popup for Myvideo.Ge', buttonEng: 'Visit', link: 'https://desp0o.github.io/channelPopUp/',
+                work1: 'Html', work2: 'CSS', work3: 'JS'
+        },
 
-    {id:17, image:`${pixnft}`, title:'pixnft.me', description:'NFT - ვებ-გვერდი, ინოვაციური გეგმებით.', button:'ნახვა',
-            descriptionEng:'NFT project Landing page, with road map and OpenSea account link.', buttonEng:'Visit', link:'https://desp0o.github.io/PixNFT/#creator',
-            work1:'HTML', work2:'CSS', work3:'JS'},
+        {
+                id: 20, image: `${shutter}`, title: 'shutter.pics', description: 'პორტფოლიო ფოტოგრაფისთვის', button: 'ნახვა',
+                descriptionEng: 'Super light portfolio for photographer with image viewer', buttonEng: 'Visit', link: 'https://meek-basbousa-38f2cc.netlify.app/',
+                work1: 'React JS', work2: 'CSS', work3: 'Tailwind', work4: 'Axios'
+        },
 
-    {id:21, image:`${moviedb}`, title:'The Movie DB', description:'React-Query -ის გამოყენებით, წამოღებულია ფილმების ბაზა, რომელზეც შეგიძლიათ ჩაატაროთ მანიპუალიცები: გაფილტროთ ჟანრისა და მომხარებლების რეიტინიგს მიხედვით, დასორტოთ და მოძებნოთ სასრუველი ფილმი ან სერიალი.', button:'ნახვა',
-            descriptionEng:'Used React-Query for fetching data. You can display trending movies of the day or week, trending TV shows, and filter movies by genres and user ratings, sort them in ascending or descending order and search for movies or TV shows by name.', buttonEng:'Visit', link:'https://meek-peony-73b096.netlify.app/',
-            work1:'React', work2:'CSS', work3:'Redux/Toolkit', work4:'React-Query'}
+        {
+                id: 19, image: `${moGraph}`, title: 'moGraph', description: 'მოუშენ დიზაინის კურსების ვებ გვერდი.', button: 'ნახვა',
+                descriptionEng: 'Web page for Courses of motion design', buttonEng: 'Visit', link: 'https://desp0o.github.io/moGraph/',
+                work1: 'HTML', work2: 'CSS', work3: 'js'
+        },
+
+        {
+                id: 10, image: `${indian}`, title: 'Moto Shop Clone', description: 'მოტოციკლეტების მაღაზიის პროექტი.', button: 'ნახვა',
+                descriptionEng: 'Moto shop web page clone, with responsive design and links animation.', buttonEng: 'Visit', link: 'https://desp0o.github.io/IndianMotorcycle/',
+                work1: 'HTML', work2: 'CSS', work3: 'JS'
+        },
+
+        {
+                id: 11, image: `${archic}`, title: 'Architect Clone', description: 'თანამედროვე,ესეთიკური, ტრადიციული ღირებულებების მქონე არქიტექტურული დიზაინები.', button: 'ნახვა',
+                descriptionEng: 'Architecutre web page clone, with responsive design and animations.', buttonEng: 'Visit', link: 'https://desp0o.github.io/Archic/',
+                work1: 'HTML', work2: 'CSS', work3: 'JS'
+        },
+
+        {
+                id: 12, image: `${chess}`, title: 'Chess Championship', description: 'ჭადრაკის ჩემპიონატის რეგისტრაციის ფორმა.', button: 'ნახვა',
+                descriptionEng: 'Chess registration form built with.', buttonEng: 'Visit', link: 'https://desp0o.github.io/RedBeryy-Chess-Hero/',
+                work1: 'React JS', work2: 'CSS'
+        },
+
+        {
+                id: 13, image: `${portfolio}`, title: 'Devweb.cc', description: 'პორტფოლიო ვებ დეველოპერისთვის.', button: 'ნახვა',
+                descriptionEng: 'Super light portfolio web page for Frontend Web Developer.', buttonEng: 'Visit', link: 'https://devweb.cc/',
+                work1: 'React JS', work2: 'CSS'
+        },
+
+        {
+                id: 16, image: `${magicMas}`, title: 'MagicMassage', description: 'მულტი ფუნქციური მასაჟორის საინფორმაციო ვებ გვერდიის კლონი.', button: 'ნახვა',
+                descriptionEng: 'MagicMassage™ provides relief from head tensions.', buttonEng: 'Visit', link: 'https://desp0o.github.io/MagicMassage/',
+                work1: 'HTML', work2: 'CSS', work3: 'JS'
+        },
+
+        {
+                id: 17, image: `${pixnft}`, title: 'pixnft.me', description: 'NFT - ვებ-გვერდი, ინოვაციური გეგმებით.', button: 'ნახვა',
+                descriptionEng: 'NFT project Landing page, with road map and OpenSea account link.', buttonEng: 'Visit', link: 'https://desp0o.github.io/PixNFT/#creator',
+                work1: 'HTML', work2: 'CSS', work3: 'JS'
+        },
+
+        {
+                id: 21, image: `${moviedb}`, title: 'The Movie DB', description: 'React-Query -ის გამოყენებით, წამოღებულია ფილმების ბაზა, რომელზეც შეგიძლიათ ჩაატაროთ მანიპუალიცები: გაფილტროთ ჟანრისა და მომხარებლების რეიტინიგს მიხედვით, დასორტოთ და მოძებნოთ სასრუველი ფილმი ან სერიალი.', button: 'ნახვა',
+                descriptionEng: 'Used React-Query for fetching data. You can display trending movies of the day or week, trending TV shows, and filter movies by genres and user ratings, sort them in ascending or descending order and search for movies or TV shows by name.', buttonEng: 'Visit', link: 'https://meek-peony-73b096.netlify.app/',
+                work1: 'React', work2: 'CSS', work3: 'Redux/Toolkit', work4: 'React-Query'
+        }
 ]
